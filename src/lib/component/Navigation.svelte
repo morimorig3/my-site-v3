1a7b5d5f189215983162bbf318ce71b9d023b898
morimorig3 <mori.nto@outlook.jp>
2023-08-16 14:13:47 +0900

    feat:ナビゲーションのアニメーション追加#21

--- src/lib/component/Navigation.svelte
+++ src/lib/component/Navigation.svelte
@@ -1,19 +1,20 @@
 <script lang="ts">
-	import { fade } from 'svelte/transition';
+	import { expoOut } from 'svelte/easing';
+	import { slide, fly } from 'svelte/transition';
 
 	export let isOpen = false;
 </script>
 
 {#if isOpen}
 	<div
-		transition:fade
+		transition:slide={{ duration: 250, easing: expoOut }}
 		class="fixed top-0 left-0 w-full h-full grid place-items-center backdrop-blur-md"
 	>
 		<nav class="max-w-screen-tablet w-full flex flex-col">
-			<a class="text-title" href="/about">ABOUT</a>
-			<a class="text-title" href="/develop">DEVELOP</a>
-			<a class="text-title" href="/projects">PROJECTS</a>
-			<a class="text-title" href="/books">BOOKS</a>
+			<a in:fly={{ delay: 250, y: -50 }} class="text-title" href="/about">ABOUT</a>
+			<a in:fly={{ delay: 300, y: -50 }} class="text-title" href="/develop">DEVELOP</a>
+			<a in:fly={{ delay: 350, y: -50 }} class="text-title" href="/projects">PROJECTS</a>
+			<a in:fly={{ delay: 400, y: -50 }} class="text-title" href="/books">BOOKS</a>
 		</nav>
 	</div>
 {/if}
