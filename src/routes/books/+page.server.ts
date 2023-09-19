2b81536417437871f6d543f0b5a97e7e8eb971d9
morimorig3 <mori.nto@outlook.jp>
2023-09-19 18:18:55 +0900

    feat:booksページのprerender#21

--- src/routes/books/+page.server.ts
+++ src/routes/books/+page.server.ts
@@ -1,6 +1,8 @@
 import { getBookList, loadBookReview } from '$lib/server/books';
 import { reviewedWith } from '$lib/server/books/functions/reviewedWith';
 
+export const prerender = true;
+
 export async function load() {
 	const { favorite, toRead, forBeginner } = await getBookList();
 	const reviewData = await loadBookReview();
