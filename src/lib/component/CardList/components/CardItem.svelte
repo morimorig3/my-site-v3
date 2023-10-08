156b9cc2a161af6026ea0a60a2523227d03882c5
morimorig3 <mori.nto@outlook.jp>
2023-10-08 13:10:18 +0900

    feat:カードコンポーネントの実装#29

---
+++ src/lib/component/CardList/components/CardItem.svelte
@@ -0,0 +1,12 @@
+<li class="item">
+	<slot />
+</li>
+
+<style lang="postcss">
+	.item {
+		@apply flex shrink-0 flex-col gap-y-1 items-center justify-center w-28 h-36 p-2 border-r;
+		&:last-child {
+			@apply border-r-0;
+		}
+	}
+</style>
