8b42aee736af7859e143b552e80b62b2641801bf
morimorig3 <mori.nto@outlook.jp>
2023-10-08 13:10:51 +0900

    refactor:layerdスタイルの共通化#29

--- src/lib/component/WorkCard.svelte
+++ src/lib/component/WorkCard.svelte
@@ -6,7 +6,7 @@
 	$: bgImage = `url(${src})`;
 </script>
 
-<a {href} class="card card--layered" style:--bg-image={bgImage}>
+<a {href} class="card u-layered-bg" style:--bg-image={bgImage}>
 	<p class="text-subtitle">{title}</p>
 	<p class="text-label-sm">{term}</p>
 	<span class="card__plus" />
@@ -16,10 +16,6 @@
 	.card {
 		min-height: 245px;
 		@apply relative p-6 text-white block shadow rounded-lg overflow-hidden;
-	}
-	.card--layered {
-		background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), var(--bg-image);
-		@apply bg-cover bg-center bg-no-repeat;
 	}
 	.card__plus {
 		@apply w-10 h-10 block absolute right-6 top-6;
--- src/routes/works/1901-coder/+page.svelte
+++ src/routes/works/1901-coder/+page.svelte
@@ -1,5 +1,121 @@
+<script lang="ts">
+	import CardList from '$lib/component/CardList/CardList.svelte';
+	import CardItem from '$lib/component/CardList/components/CardItem.svelte';
+	import FaBuildings from '$lib/component/Icons/FaBuildings.svelte';
+	import FaCode from '$lib/component/Icons/FaCode.svelte';
+	import FaDev from '$lib/component/Icons/FaDev.svelte';
+	import FaLocationDot from '$lib/component/Icons/FaLocationDot.svelte';
+	import FaPeopleGroup from '$lib/component/Icons/FaPeopleGroup.svelte';
+	import SiAdobeIllustrator from '$lib/component/Icons/SiAdobeIllustrator.svelte';
+	import SiCss3 from '$lib/component/Icons/SiCss3.svelte';
+	import SiHtml5 from '$lib/component/Icons/SiHtml5.svelte';
+	import TabButton from '$lib/component/TabButton.svelte';
+
+	const TAB_LIST = ['PR', '概要'];
+
+	const src = '/works_web-engineer_01.jpg';
+	$: bgImage = `url(${src})`;
+	let activeTabIndex = 0;
+	function setActiveTabIndex(index: number) {
+		activeTabIndex = index;
+	}
+</script>
+
 <div class="l-container py-6 tablet:py-8 laptop:py-10">
-	<h1 class="text-subtitle">コーダー</h1>
+	<div
+		class="u-negative-container hero u-layered-bg mb-6 flex flex-col items-center justify-center shadow"
+		style:--bg-image={bgImage}
+	>
+		<h1 class="text-subtitle text-white">コーダー</h1>
+		<p class="text-label-sm text-white">Jan. 2019 ~ Sep. 2019</p>
+	</div>
+	<div class="flex flex-col gap-y-6">
+		<section>
+			<h2 class="text-subtitle mb-4">Stacks</h2>
+			<CardList>
+				<CardItem>
+					<SiHtml5 className="h-8" />
+					<p class="text-label font-bold">HTML</p>
+				</CardItem>
+				<CardItem>
+					<SiCss3 className="h-8" />
+					<p class="text-label font-bold">CSS</p>
+				</CardItem>
+				<CardItem>
+					<SiAdobeIllustrator className="h-8" />
+					<p class="text-label font-bold">Illustrator</p>
+				</CardItem>
+			</CardList>
+		</section>
+		<section>
+			<h2 class="text-subtitle mb-4">Features</h2>
+			<CardList>
+				<CardItem>
+					<FaLocationDot className="h-6" />
+					<p class="text-label-sm text-placeholder">勤務地</p>
+					<p class="text-label font-bold">大阪</p>
+				</CardItem>
+				<CardItem>
+					<FaBuildings className="h-6" />
+					<p class="text-label-sm text-placeholder">勤務形態</p>
+					<p class="text-label font-bold">オフィス</p>
+				</CardItem>
+				<CardItem>
+					<FaDev className="h-6" />
+					<p class="text-label-sm text-placeholder">開発体制</p>
+					<p class="text-label font-bold text-center">ウォーター<br />フォール</p>
+				</CardItem>
+				<CardItem>
+					<FaPeopleGroup className="h-6" />
+					<p class="text-label-sm text-placeholder">チーム</p>
+					<p class="text-label font-bold">1人</p>
+				</CardItem>
+				<CardItem>
+					<FaCode className="h-6" />
+					<p class="text-label-sm text-placeholder">主要スキル</p>
+					<p class="text-label font-bold">CSS</p>
+				</CardItem>
+			</CardList>
+		</section>
+		<section>
+			<div
+				role="tablist"
+				class="w-full grid auto-cols-fr grid-flow-col rounded-lg border shadow bg-white"
+			>
+				{#each TAB_LIST as tab, index}
+					<TabButton
+						handleClick={() => setActiveTabIndex(index)}
+						label={tab}
+						isActive={activeTabIndex === index}
+					/>
+				{/each}
+			</div>
+			{#each TAB_LIST as tab, index}
+				{#if activeTabIndex === index}
+					<p>{tab}</p>
+				{/if}
+			{/each}
+		</section>
+	</div>
+	<p>技術スタック</p>
+	<ul>
+		<li>必要か？</li>
+	</ul>
+
+	<p>アイコンで表現</p>
+	<ul>
+		<li>開発体制</li>
+		<li>開発手法</li>
+		<li>主要スキル</li>
+		<li>勤務地</li>
+	</ul>
+
+	<p>文章で表現</p>
+	<ul>
+		<li>概要</li>
+		<li>PR</li>
+	</ul>
+
 	<p class="text-label-sm">Jan. 2019 ~ Sep. 2019（9ヶ月間）</p>
 	<h2 class="text-subtitle">なにをしたか</h2>
 	<p class="text-body-sm">
@@ -19,7 +135,6 @@
 
 	<section>
 		<h1 class="text-title">コーダー</h1>
-		<p class="text-label-sm text-placeholder">Jan. 2019 ~ Sep. 2019（9ヶ月間）</p>
 		<p>百貨店・商業施設</p>
 		<h2>働き方</h2>
 		<h3>チーム</h3>
@@ -36,3 +151,10 @@
 		<p>静的なhtmlとcssがメインでインタラクティブの表現にはjQueryを使用していました。</p>
 	</section>
 </div>
+
+<style lang="postcss">
+	.hero {
+		height: 56vw;
+		max-height: 293px;
+	}
+</style>
