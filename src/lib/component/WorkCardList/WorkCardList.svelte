4b3d5541845afb0255a8806938052d35ec1f50b2
morimorig3 <mori.nto@outlook.jp>
2023-10-14 12:29:25 +0900

    feat:workCardをlaptop以上でも2カラムにする#36

--- src/lib/component/WorkCardList/WorkCardList.svelte
+++ src/lib/component/WorkCardList/WorkCardList.svelte
@@ -1,16 +1,3 @@
-<script lang="ts">
-	export let isLarge = false;
-</script>
-
-<ul
-	class:isLarge
-	class="grid tablet:grid-cols-2 laptop:grid-cols-3 gap-6 tablet:gap-8 laptop:gap-10"
->
+<ul class="grid laptop:grid-cols-2 gap-6 tablet:gap-8 laptop:gap-10">
 	<slot />
 </ul>
-
-<style lang="postcss">
-	.isLarge {
-		@apply grid tablet:grid-cols-1 laptop:grid-cols-2;
-	}
-</style>
