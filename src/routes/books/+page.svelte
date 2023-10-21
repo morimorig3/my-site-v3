ccc5ca2b52eaec5f765d836aaf76188c0d5e7704
morimorig3 <mori.nto@outlook.jp>
2023-10-21 16:27:19 +0900

    feat:Booksのダークテーマ適用#42

--- src/routes/books/+page.svelte
+++ src/routes/books/+page.svelte
@@ -23,10 +23,10 @@
 	pathName={$page.url.pathname}
 />
 
-<div class="bg-lightTertiary">
-	<div class="shadow-md bg-white">
+<div class="bg-lightTertiary dark:bg-lightPrimary">
+	<div class="shadow-md bg-white dark:bg-lightSecondary">
 		<div class="l-container py-6 tablet:py-8 laptop:py-10 mb-6">
-			<h1 class="text-heading-1 mb-6">{favoriteBookShelves.title}</h1>
+			<h1 class="text-heading-1 mb-6 dark:text-lightWhite">{favoriteBookShelves.title}</h1>
 			<BookListLarge bookItems={favoriteBookShelves.bookList} />
 		</div>
 	</div>
@@ -34,7 +34,7 @@
 		{#if !$isDeskTop}
 			<div
 				role="tablist"
-				class="w-full grid auto-cols-fr grid-flow-col rounded-lg border shadow bg-white"
+				class="w-full grid auto-cols-fr grid-flow-col rounded-lg overflow-hidden border shadow bg-white dark:bg-lightBody dark:border-black"
 			>
 				{#each bookShelves as { title }, index}
 					<TabButton
@@ -49,7 +49,7 @@
 			{#each bookShelves as { bookList, title }, index (title)}
 				{#if $isDeskTop}
 					<div class="flex-shrink-0 w-80">
-						<p class="text-body font-bold">{title}</p>
+						<p class="text-body font-bold dark:text-lightWhite">{title}</p>
 						<BookListSmall bookItems={bookList} />
 					</div>
 				{:else if activeTabIndex === index}
