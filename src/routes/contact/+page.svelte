a90ee7ce2e48f7975a30761c084bfc45e5dd436b
morimorig3 <mori.nto@outlook.jp>
2023-11-18 11:24:00 +0900

    frat:コンタクトフォームのデザイン実装#50

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -10,33 +10,48 @@
 />
 
 <div class="l-container py-6 tablet:py-8 laptop:py-10">
-	<form method="POST" action="/">
+	<form class="max-w-screen-tablet mx-auto flex flex-col gap-y-8" method="POST" action="/">
 		<div class="floating-input">
-			<input class="floating-input__input" type="text" id="name" placeholder="Name" />
+			<input
+				class="floating-input__input"
+				type="text"
+				id="name"
+				placeholder="Name"
+				autocomplete="off"
+			/>
 			<label class="floating-input__label" for="Name">Name</label>
 		</div>
 		<div class="floating-input">
-			<input class="floating-input__input" type="text" id="email" placeholder="E-mail" />
+			<input
+				class="floating-input__input"
+				type="text"
+				id="email"
+				placeholder="E-mail"
+				autocomplete="off"
+			/>
 			<label class="floating-input__label" for="E-mail">E-mail</label>
 		</div>
-		<label>
-			内容
-			<textarea name="message" />
-		</label>
-		<button>送信</button>
+		<textarea class="textarea" name="message" placeholder="本文" />
+		<button
+			class="w-full text-label font-bold border shadow rounded-md bg-white h-12 relative hover:bg-sky-50 dark:bg-divider dark:border-0 dark:hover:bg-lightBody"
+			>確認</button
+		>
 	</form>
 </div>
 
 <style lang="postcss">
 	.floating-input {
-		@apply relative;
+		@apply relative dark:text-lightBody;
 		&__input {
-			@apply w-full h-full pb-2 pt-4 border-b;
+			@apply w-full h-full p-2 pt-4 border-b bg-transparent dark:border-placeholder;
 			&:focus {
-				@apply border-body outline-none;
+				@apply border-body outline-none dark:border-white;
 			}
 			&::placeholder {
 				color: transparent;
+			}
+			&:-internal-autofill-selected {
+				@apply bg-transparent;
 			}
 			&:focus + .floating-input__label,
 			&:not(:placeholder-shown) + .floating-input__label {
@@ -51,6 +66,17 @@
 			pointer-events: none;
 			transform-origin: left center;
 			transition: transform 250ms;
+		}
+	}
+
+	.textarea {
+		min-height: 200px;
+		@apply border rounded-md w-full p-2 bg-transparent dark:text-lightBody dark:border-placeholder;
+		&::placeholder {
+			@apply text-placeholder;
+		}
+		&:focus {
+			@apply border-body outline-none dark:border-white;
 		}
 	}
 </style>
