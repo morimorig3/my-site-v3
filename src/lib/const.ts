cad51c99e92f8ae66ee17908354c16cbed4557cb
morimorig3 <mori.nto@outlook.jp>
2024-08-24 11:30:54 +0900

    fest:Historyに新しい歴史を追加

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -3,6 +3,7 @@ import FaCar from './component/Icons/FaCar.svelte';
 import FaCode from './component/Icons/FaCode.svelte';
 import FaDev from './component/Icons/FaDev.svelte';
 import FaFlag from './component/Icons/FaFlag.svelte';
+import FaJava from './component/Icons/FaJava.svelte';
 import FaLocationDot from './component/Icons/FaLocationDot.svelte';
 import FaPeopleGroup from './component/Icons/FaPeopleGroup.svelte';
 import FaPhone from './component/Icons/FaPhone.svelte';
@@ -10,8 +11,10 @@ import FaScrewDriverWrench from './component/Icons/FaScrewDriverWrench.svelte';
 import SiAdobeDreamWeaver from './component/Icons/SiAdobeDreamWeaver.svelte';
 import SiAdobeIllustrator from './component/Icons/SiAdobeIllustrator.svelte';
 import SiAdobePhotoShop from './component/Icons/SiAdobePhotoShop.svelte';
+import SiAmazonWebServices from './component/Icons/SiAmazonWebServices.svelte';
 import SiCss3 from './component/Icons/SiCss3.svelte';
 import SiGatsby from './component/Icons/SiGatsby.svelte';
+import SiGit from './component/Icons/SiGit.svelte';
 import SiGitLab from './component/Icons/SiGitLab.svelte';
 import SiGo from './component/Icons/SiGo.svelte';
 import SiGulp from './component/Icons/SiGulp.svelte';
@@ -19,12 +22,14 @@ import SiHtml5 from './component/Icons/SiHtml5.svelte';
 import SiJQuery from './component/Icons/SiJQuery.svelte';
 import SiJs from './component/Icons/SiJs.svelte';
 import SiNext from './component/Icons/SiNext.svelte';
+import SiPostgresql from './component/Icons/SiPostgresql.svelte';
 import SiReact from './component/Icons/SiReact.svelte';
 import SiRealm from './component/Icons/SiRealm.svelte';
 import SiRedmine from './component/Icons/SiRedmine.svelte';
 import SiRedux from './component/Icons/SiRedux.svelte';
 import SiSass from './component/Icons/SiSass.svelte';
 import SiSvelte from './component/Icons/SiSvelte.svelte';
+import SiTerraform from './component/Icons/SiTerraform.svelte';
 import SiTs from './component/Icons/SiTs.svelte';
 import SiWordPress from './component/Icons/SiWordPress.svelte';
 
@@ -69,6 +74,7 @@ export const ICON_MAP = {
 	SiTs: SiTs,
 	SiRedux: SiRedux,
 	SiGitLab: SiGitLab,
+	SiGit: SiGit,
 	SiRealm: SiRealm,
 	SiRedmine: SiRedmine,
 	SiWordPress: SiWordPress,
@@ -80,6 +86,10 @@ export const ICON_MAP = {
 	SiNext: SiNext,
 	SiGatsby: SiGatsby,
 	SiSvelte: SiSvelte,
+	FaJava: FaJava,
+	SiAmazonWebServices: SiAmazonWebServices,
+	SiPostgresql: SiPostgresql,
+	SiTerraform: SiTerraform,
 	default: FaFlag
 } as const;
 
